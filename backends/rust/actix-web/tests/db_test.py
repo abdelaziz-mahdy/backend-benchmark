ef57389dc6cb708f42dcd4895948ed0d9ef2ce83
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-02-11 01:29:31 +0200

    fix wrong status code

--- backends/rust/actix-web/tests/db_test.py
+++ backends/rust/actix-web/tests/db_test.py
@@ -9,7 +9,7 @@ class NoteUser(FastHttpUser):
             "title": "Sample Note",
             "content": "This is a note content."
         }, catch_response=True) as response:
-            if response.status_code != 200:
+            if response.status_code != 201:
                 error_message = f"BadStatusCode: '{response.url}', code={response.status_code}, response_text={response.text}"
                 response.failure(error_message)
                 # Rethrow with added response text
