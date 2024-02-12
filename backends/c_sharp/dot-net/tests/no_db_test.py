5b8a63aa3bbc1d1aad5fccf74f22f8d96598b1db
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-02-13 00:52:44 +0200

    fix and adding results

--- backends/c_sharp/dot-net/tests/no_db_test.py
+++ backends/c_sharp/dot-net/tests/no_db_test.py
@@ -4,12 +4,12 @@ class NoteUser(FastHttpUser):
 
     @task
     def no_db_endpoint(self):
-        with self.client.get("/api/notes/no_db_endpoint/", catch_response=True) as response:
+        with self.client.get("/notes/no_db_endpoint/", catch_response=True) as response:
             if response.status_code != 200:
                 response.failure(f"Unexpected status code: {response.status_code}. Response: {response.text}")
 
     @task
     def no_db_endpoint2(self):
-        with self.client.get("/api/notes/no_db_endpoint2/", catch_response=True) as response:
+        with self.client.get("/notes/no_db_endpoint2/", catch_response=True) as response:
             if response.status_code != 200:
                 response.failure(f"Unexpected status code: {response.status_code}. Response: {response.text}")
