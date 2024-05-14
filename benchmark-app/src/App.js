7c88a193a91d2adf251344cb4738741efb079e79
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-14 17:48:00 +0300

    improve ui

--- benchmark-app/src/App.js
+++ benchmark-app/src/App.js
@@ -1,9 +1,20 @@
-// src/App.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 import './App.css'; // Import CSS for styling
+
+const colorMap = {
+  db_test: '#FF33A8',
+  no_db_test: '#333FA8',
+};
+
+const getColor = (serviceName) => {
+  if (serviceName.toLowerCase().includes('no_db_test')) return colorMap.no_db_test;
+  if (serviceName.toLowerCase().includes('db_test')) return colorMap.db_test;
+  const baseServiceName = serviceName.toLowerCase().replace('no_db_test', '').replace('db_test', '').trim();
+  return colorMap[baseServiceName] || `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
 function App() {
   const [data, setData] = useState({});
@@ -54,45 +65,57 @@ function App() {
     <div className="App">
       <header>
         <h1>Service Benchmarks</h1>
-        <div className="service-selection">
-          <div className="services">
-            <label>Select Services:</label>
-            {Object.keys(data).map(service => (
-              <div key={service}>
-                <input
-                  type="checkbox"
-                  value={service}
-                  onChange={handleServiceChange}
-                  checked={selectedServices.includes(service)}
-                />
+        <div className="legend">
+          <h2>Legend</h2>
+          <ul>
+            {Object.entries(colorMap).map(([service, color]) => (
+              <li key={service} style={{ color }}>
                 {service}
-              </div>
+              </li>
             ))}
-          </div>
-          {selectedServices.length > 0 && (
-            <div className="fields">
-              <label>Select Fields for Y-axis:</label>
-              {Object.keys(data[selectedServices[0]][0])
-                .filter(field => field !== 'Timestamp') // Exclude Timestamp from selection
-                .map(field => (
-                  <div key={field}>
-                    <input
-                      type="checkbox"
-                      value={field}
-                      onChange={handleFieldChange}
-                      checked={selectedFields.includes(field)}
-                    />
-                    {field}
-                  </div>
-                ))}
-            </div>
-          )}
+          </ul>
         </div>
       </header>
-      <div className="chart-container">
-        {selectedServices.length > 0 && selectedFields.length > 0 && (
-          <Line data={generateChartData()} />
+      <div className="main-content">
+        <div className="services">
+          <label>Select Services:</label>
+          {Object.keys(data).map(service => (
+            <div key={service}>
+              <input
+                type="checkbox"
+                value={service}
+                onChange={handleServiceChange}
+                checked={selectedServices.includes(service)}
+              />
+              <span style={{ color: getColor(service) }}>
+                {service.replace('no_db_test', '').replace('db_test', '').trim()}
+              </span>
+            </div>
+          ))}
+        </div>
+        {selectedServices.length > 0 && (
+          <div className="fields">
+            <label>Select Fields for Y-axis:</label>
+            {Object.keys(data[selectedServices[0]][0])
+              .filter(field => field !== 'Timestamp') // Exclude Timestamp from selection
+              .map(field => (
+                <div key={field}>
+                  <input
+                    type="checkbox"
+                    value={field}
+                    onChange={handleFieldChange}
+                    checked={selectedFields.includes(field)}
+                  />
+                  {field}
+                </div>
+              ))}
+          </div>
         )}
+        <div className="chart-container">
+          {selectedServices.length > 0 && selectedFields.length > 0 && (
+            <Line data={generateChartData()} />
+          )}
+        </div>
       </div>
     </div>
   );
