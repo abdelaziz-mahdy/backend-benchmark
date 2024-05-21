b4376652db9485359a34486e4cea3ffd43057f60
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-21 14:47:35 +0300

    trying with gunicorn, results are bad

--- backends/python/django-sync/benchmark/run_only_for_docker.sh
+++ backends/python/django-sync/benchmark/run_only_for_docker.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+
 cd "${0%/*}"
 python manage.py makemigrations
 python manage.py migrate
-(trap 'kill 0' SIGINT; gunicorn benchmark.wsgi & wait)
+
+# Determine the number of CPU cores
+CPU_CORES=$(nproc)
+
+# Gunicorn configuration for I/O-bound applications using gevent
+WORKERS=$((CPU_CORES * 2 + 1))
+# WORKER_CLASS="gevent"
+THREADS=$((CPU_CORES * 2 + 1))
+WORKER_CONNECTIONS=1000
+BIND="0.0.0.0:8000"
+LOG_LEVEL="info"
+
+
+(trap 'kill 0' SIGINT; gunicorn benchmark.wsgi:application \
+    --workers $WORKERS \
+    --threads $THREADS \
+    --worker-connections $WORKER_CONNECTIONS \
+    --bind $BIND \
+    --log-level $LOG_LEVEL \
+ & wait)
